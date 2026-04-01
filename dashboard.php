78e0c6f2abccdaee6170fae8159a6f854ee4b383
Marwan <marwan2k3@gmail.com>
2026-04-01 17:31:37 +0300

    Initial commit

---
+++ dashboard.php
@@ -0,0 +1,15 @@
+<?php include 'db_connect.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+    <h2>Student Dashboard</h2>
+
+    <a href="upload_result.php">Upload Results</a><br>
+    <a href="course_structure.php">View Course Structure</a><br>
+    <a href="course_planning.php">Plan Courses</a><br>
+</body>
+</html>
